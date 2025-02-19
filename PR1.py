4b99c0e154a820eee701b58e402bff03d89b1657
AKALABOR <110409066+AKALABOR@users.noreply.github.com>
2025-02-19 10:24:07 +0200

    Create PR1.py

---
+++ PR1.py
@@ -0,0 +1,9 @@
+print("Hello, Python!")
+
+name = input("Введіть своє ім'я- ")
+print(f"Привіт, {name}!")
+
+num1 = float(input("Введіть перше число- "))
+num2 = float(input("Введіть друге число- "))
+sum_result = num1 + num2
+print(f"Сума чисел {num1} і {num2} дорівнює {sum_result}")
