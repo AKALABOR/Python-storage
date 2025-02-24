5a20cc5f4916bfbf3bccd5d7716169154485917d
AKALABOR <110409066+AKALABOR@users.noreply.github.com>
2025-02-24 22:52:34 +0200

    Update quote_fetcher(lab1.1).py

--- quote_fetcher(lab1.1).py
+++ quote_fetcher(lab1.1).py
@@ -1,18 +1,19 @@
 import requests
 
-url = "https://api.quotable.io/random"
+url = "https://favqs.com/api/qotd"
 
-response = requests.get(url)
-
-if response.status_code == 200:
-
-    data = response.json()
-    quote = data["content"]
-    author = data["author"]
+try:
+    response = requests.get(url)
+    response.raise_for_status()
     
+    data = response.json()
+    quote = data["quote"]["body"]
+    author = data["quote"]["author"]
+
     print(f'"{quote}" - {author}')
 
     with open("quote.txt", "w", encoding="utf-8") as file:
         file.write(f'"{quote}" - {author}')
-else:
-    print("Помилка отримання цитати")
+
+except requests.exceptions.RequestException as e:
+    print("Помилка під час отримання цитати:", e)
