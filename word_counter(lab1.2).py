24997a62ed8c4a0ee9ce03591e301018bd6eae5d
AKALABOR <110409066+AKALABOR@users.noreply.github.com>
2025-02-24 22:52:19 +0200

    Update word_counter(lab1.2).py

--- word_counter(lab1.2).py
+++ word_counter(lab1.2).py
@@ -1,14 +1,13 @@
-def count_words(filename):
-    """Функція підраховує кількість слів у текстовому файлі."""
+def count_words_in_file(filename):
     try:
         with open(filename, "r", encoding="utf-8") as file:
             text = file.read()
             words = text.split()
             return len(words)
     except FileNotFoundError:
-        print("Файл не знайдено.")
+        print(f"Файл '{filename}' не знайдено.")
         return 0
 
 filename = "quote.txt"
-word_count = count_words(filename)
+word_count = count_words_in_file(filename)
 print(f"Кількість слів у файлі '{filename}': {word_count}")
